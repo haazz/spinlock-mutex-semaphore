a3911768bec80d96ab67a86d44785af29f3cd73a
haazz <gkwldnjs121@gmail.com>
2025-09-05 12:19:16 +0900

    feat: Execute three type of lock

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,18 @@
 package com.example;
 
+import com.example.mutex.Mutex;
+import com.example.semaphore.BinarySemaphore;
+import com.example.spinlock.SpinLock;
+
 public class Main {
+    
+    public static void main(String[] args) throws InterruptedException {
+        final Executor spinLockExecutor = new Executor(new SpinLock());
+        final Executor mutexExecutor = new Executor(new Mutex());
+        final Executor binarySemaphoreExecutor = new Executor(new BinarySemaphore());
 
-    public static void main(String[] args) {
-
+        spinLockExecutor.run();
+        mutexExecutor.run();
+        binarySemaphoreExecutor.run();
     }
 }
